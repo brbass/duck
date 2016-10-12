12ad0214b81d6260a07b1bb6c2f35d82e10edd48
Brody Bassett <brbass@umich.edu>
2016-10-12 00:47:33 -0400

    Can't find error in derivatives for MLS functions, but added plotting to see if that will help.

--- src/basis.py
+++ src/basis.py
@@ -173,98 +173,114 @@ class MLS:
         self.num_points = len(points)
         self.dx = points[1] - points[0]
         self.bandwidth = 1. / (self.dx * (self.num_other_points + 0.1))
-    def weight(d):
+    def weightd(self,
+               d):
         if d <= 1:
             return np.power(1. - d, 3) * (1. + 3. * d)
         else:
             return 0.
-    def dweight(d):
+    def dweightd(self,
+                 d):
         if d <= 1:
             return -12 * self.bandwidth * np.power(d - 1, 2) * d
         else:
             return 0.
-    def weight(i,
+    def weight(self,
+               i,
                x):
-        return self.weight(self.bandwidth * np.abs(x - self.points[i]))
-    def dweight(i,
+        return self.weightd(self.bandwidth * np.abs(x - self.points[i]))
+    def dweight(self,
+                i,
                 x):
-        return self.dweight(self.bandwidth * np.abs(x - self.points[i]))
-    def get_points(x):
-        nearest_point = np.round(x / self.dx)
+        return self.dweightd(self.bandwidth * np.abs(x - self.points[i]))
+    def get_points(self,
+                   x):
+        nearest_point = int(np.round(x / self.dx))
         local_points = []
         for i in range(nearest_point - self.num_other_points - 1, nearest_point + self.num_other_points + 1):
             if (i >= 0 and i < self.num_points
-                and np.abs(x - self.points[i]) < bandwidth):
+                and np.abs(x - self.points[i]) < self.bandwidth):
                 local_points.append([i])
         return np.array(local_points, dtype=int)
-    def amat(x):
+    def amat(self,
+             x):
         local_points = self.get_points(x)
         mat = np.zeros((self.num_polynomials, self.num_polynomials))
         
         for i in local_points:
-            w = weight(i,
-                       x)
+            w = self.weight(i,
+                            x)
             for j in range(self.num_polynomials):
                 for k in range(self.num_polynomials):
                     mat[j, k] = mat[j, k] + np.power(self.points[i], j + k) * w
         return mat
-    def damat(x):
+    def damat(self,
+              x):
         local_points = self.get_points(x)
         mat = np.zeros((self.num_polynomials, self.num_polynomials))
         
         for i in local_points:
-            dw = dweight(i,
-                         x)
+            dw = self.dweight(i,
+                              x)
             for j in range(self.num_polynomials):
                 for k in range(self.num_polynomials):
                     mat[j, k] = mat[j, k] + np.power(self.points[i], j + k) * dw
         return mat
-    def ainv(x):
-        return np.linalg.inverse(amat(x))
-    def dainv(x):
-        ainvval = ainv(x)
-        daval = damat(x)
+    def ainv(self,
+             x):
+        return np.linalg.inv(self.amat(x))
+    def dainv(self,
+              x):
+        ainvval = self.ainv(x)
+        daval = self.damat(x)
         return -1. * np.dot(ainvval, np.dot(daval, ainvval))
-    def poly(x):
+    def poly(self,
+             x):
         vec = np.zeros(self.num_polynomials)
         for i in range(self.num_polynomials):
             vec[i] = np.power(x, i)
         return vec
-    def dpoly(x):
+    def dpoly(self,
+              x):
         vec = np.zeros(self.num_polynomials)
         for i in range(1, self.num_polynomials):
             vec[i] = i * np.power(x, i - 1)
         return vec
-    def bvec(i,
+    def bvec(self,
+             i,
              x):
-        return poly(points[i]) * weight(i,
-                                        x)
-    def dbvec(i,
+        return self.poly(points[i]) * self.weight(i,
+                                                  x)
+    def dbvec(self,
+              i,
               x):
-        return poly(points[i]) * dweight(i,
-                                         x)
+        return self.poly(points[i]) * self.dweight(i,
+                                                   x)
         
     def val(self,
             i,
             x):
-        return np.dot(poly(x), np.dot(ainv(x), bvec(i,
-                                                    x)))
+        polyval = self.poly(x)
+        ainvval = self.ainv(x)
+        bvecval = self.bvec(i,
+                            x)
+        return np.dot(polyval, np.dot(ainvval, bvecval))
     def dval(self,
              i,
              x):
-        polyval = poly(x)
-        dpolyval = dpoly(x)
-        aval = amat(x)
-        daval = damat(x)
-        ainvval = np.linalg.inverse(amat(x))
-        bval = bvec(i,
-                    x)
-        dbval = dbvec(i,
-                      x)
+        polyval = self.poly(x)
+        dpolyval = self.dpoly(x)
+        aval = self.amat(x)
+        daval = self.damat(x)
+        ainvval = np.linalg.inv(aval)
+        bval = self.bvec(i,
+                         x)
+        dbval = self.dbvec(i,
+                           x)
         dainvval = -np.dot(ainvval, np.dot(daval, ainvval))
         t1 = np.dot(dpolyval, np.dot(ainvval, bval))
-        t2 = np.dot(polyval, dainvval, bval)
-        t3 = np.dot(polyval, ainvval, dbval)
+        t2 = np.dot(polyval, np.dot(dainvval, bval))
+        t3 = np.dot(polyval, np.dot(ainvval, dbval))
         return t1 + t2 + t3
 
 class Constant(Compact_RBF):
@@ -295,24 +311,28 @@ class Constant(Compact_RBF):
             return 0.
 
 if __name__ == '__main__':
-    points = np.linspace(0, 1, 3)
+    points = np.linspace(0, 1, 10)
     shape = 2.0
     sigma_t = Cross_Section(1.0,
                             2.0)
-    funcs = [Wendland(shape,
-                      points),
-             Multiquadric(shape,
-                          points),
-             Gaussian(shape,
-                      points),
-             Constant(shape,
-                      points),
-             Compact_Gaussian(shape,
-                              points),
-             SUPG_Gaussian(shape,
-                           points,
-                           sigma_t)]
-    desc = ["wend", "mult", "gauss", "const", "com_gauss", "supg_gauss"]
+    # funcs = [Wendland(shape,
+    #                   points),
+    #          Multiquadric(shape,
+    #                       points),
+    #          Gaussian(shape,
+    #                   points),
+    #          Constant(shape,
+    #                   points),
+    #          Compact_Gaussian(shape,
+    #                           points),
+    #          SUPG_Gaussian(shape,
+    #                        points,
+    #                        sigma_t)]
+    # desc = ["wend", "mult", "gauss", "const", "com_gauss", "supg_gauss"]
+    funcs = [MLS(2,
+                 3,
+                 points)]
+    desc = ["mls"]
     col = ['#66c2a5','#fc8d62','#8da0cb','#e78ac3','#a6d854', 'k']
     plot_points = np.linspace(0, 1, 200)
     plt.ylim(0, 2)
